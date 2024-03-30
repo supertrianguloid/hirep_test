da8e4c801034617276d9ece4de04571df9ce8d8d
Laurence Sebastian Bowes <github@supertrianguloid.com>
2024-03-30 18:04:26 +0000

    New compile script

--- hirep_test.jl
+++ hirep_test.jl
@@ -2,25 +2,29 @@
 using YAML
 
 REPO = "https://github.com/claudiopica/HiRep"
-TEMP_FOLDER = "./hirep_test_temp/"
+TEST_FOLDER = "./hirep_tests_running/"
 
 recipes = YAML.load_file("recipes.yaml")
 @show recipes
 
-mkdir(TEMP_FOLDER)
-cd(TEMP_FOLDER)
+mkdir(TEST_FOLDER)
+cd(TEST_FOLDER)
 run(`git clone $REPO`)
 
 for recipe in recipes
     name, keys = recipe
-    cp("HiRep", name)
+    cp("./HiRep", name)
     cd(name)
-    run(`module purge`)
-    for mod in keys["modules"]
-        run(`module load $mod`)
-    end
-    cp("../../MkFlags.template", "./Make/MkFlags")
+    cp("../../MkFlags.template", "./Make/MkFlags", force=true)
+    flag_file = open("./Make/MkFlags", "a")
     for flag in ["CFLAGS", "CC", "MPICC"]
-        write(open("./Make/MkFlags", "a"), "$(keys[flag])\n")
+        write(flag_file, "$flag = $(keys[flag])\n")
     end
+    script = open("./compile_script.sh", "w")
+    write(script, "#!/bin/bash\nmodule purge\n")
+    for mod in keys["modules"]
+        write(script, "module load $mod\n")
+    end
+    write(script, read(open("../../compile_script.sh")))
+    cd("../")
 end
